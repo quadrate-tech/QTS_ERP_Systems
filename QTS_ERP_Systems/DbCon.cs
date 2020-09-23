fe719424fff0b4a5d4008fba7779b143b5366828
M F M Fazrin <mfmfazrin1986@gmail.com>
2020-09-23 18:17:17 +0530

    Commit Mogodb Updates

---
+++ QTS_ERP_Systems/DbCon.cs
@@ -0,0 +1,21 @@
+﻿using MongoDB.Driver;
+
+namespace QTS_ERP_Systems
+{
+    class DbCon
+    {
+        private IMongoDatabase db;
+
+        public DbCon(string database)
+        {
+            var client = new MongoClient();
+            db = client.GetDatabase(database);
+        }
+
+        public void InsertRecord<T>(string table, T record)
+        {
+            var Collection = db.GetCollection<T>(table);
+            Collection.InsertOne(record);
+        }
+    }
+}
