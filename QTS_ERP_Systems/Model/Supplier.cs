b86a09037d3ef77706323aa5309305cd809c22f2
shanthu-hammer <55648960+shanthu-hammer@users.noreply.github.com>
2020-10-15 00:36:04 +0800

    DBcon delete &model error fix

--- QTS_ERP_Systems/Model/Supplier.cs
+++ QTS_ERP_Systems/Model/Supplier.cs
@@ -1,12 +1,26 @@
-﻿using System;
+﻿using MongoDB.Bson.Serialization.Attributes;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace QTS_ERP_Systems.Model
 {
-    class SimpleBilling
+    [BsonIgnoreExtraElements]
+    class Supplier
     {
+        public int SupplierId { get; set; }
+        
+  
+        public string Name { get; set; }
+
+        [Required]
+        public string Contact { get; set; }
+        public string Address { get; set; }
+        [EmailAddress]
+        public string Email { get; set; }
+        public float PendingAmount { get; set; }
     }
 }
